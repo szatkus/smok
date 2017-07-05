210565cd5a5ea71ddeb8aa1c32415cfd86ced95f
terrorystkaf <aleksandrabazylewicz@gmail.com>
2017-07-05 18:49:01 +0200

    school app remove

--- subjects/school/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import School
-
-class SchoolForm(forms.ModelForm):
-
-    class Meta:
-        model = School
-        fields = ('school_name', 'school_address', 'school_type',)
