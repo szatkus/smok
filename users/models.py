f91572b5d6a241463b5d610d48a2857183e04ac2
terrorystkaf <aleksandrabazylewicz@gmail.com>
2017-07-05 18:15:12 +0200

    Dodane pole school id

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models.fields import CharField
-
+from django.db import models
+from school.models import  School
 
 class User(AbstractUser):
-    remove_me = CharField(max_length=255)
+    remove_me = CharField(max_length=255)    
+    school_id = models.ManyToManyField(School)
