aa20ea9798e96c85ab78ca4352b7c7ac8042e0b8
surbanski <s.urbanski.h@gmail.com>
2017-07-17 19:59:12 +0200

    TSMOK-24: Dodana ogólna funkcja do usuwania zasobu

--- commons/static/js/base_resources_list.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-    // Add a "checked" symbol when clicking on a list item
-    var list = document.querySelector('ul#list');
-    list.addEventListener('click', function(ev) {
-      if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-      }
-    }, false);
-});
---
+++ subjects/static/js/subjects.js
@@ -0,0 +1,2 @@
+
+var csrftoken = getCookie('csrftoken');
