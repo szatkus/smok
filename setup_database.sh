93bdf49f55da545873f13d9bda5f54c88316eb17
surbanski <s.urbanski.h@gmail.com>
2017-09-27 23:29:32 +0200

    TSMOK-58: Dodane dane testowe dla profilow grup

--- setup_database.sh
+++ setup_database.sh
@@ -8,7 +8,9 @@ pushd $SCRIPTPATH
 find . -name migrations -type d -not -path "./venv/*" -exec rm -r {} \; 2>/dev/null
 [ -f db.sqlite3 ] && rm db.sqlite3
 
-venv/bin/python manage.py makemigrations school users subjects groups teachers classrooms availability
+venv/bin/python manage.py makemigrations school users subjects groups teachers classrooms availability classProfiles
 venv/bin/python manage.py migrate
 venv/bin/python manage.py loaddata testdata.json
+echo "Loaded test data"
 echo "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin', 'admin@example.com', 'pass')" | venv/bin/python manage.py shell
+echo "Created superuser with login: admin and password: pass"
