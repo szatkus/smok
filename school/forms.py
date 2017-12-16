92a766d3d983b9d7a8bac721f527cba2633c51e3
terrosytskaf <aleksandrabazylewicz@gmail.com>
2017-12-16 08:44:08 +0100

    Aplikacja szkola dostosowana do nowych wymagan

--- school/forms.py
+++ school/forms.py
@@ -7,3 +7,7 @@ class SchoolForm(forms.ModelForm):
     class Meta:
         model = School
         fields = ('school_name', 'school_address', 'school_type',)
+        widgets = {
+            'school_name': forms.TextInput(attrs={'class': 'input-field'}),
+            'school_address': forms.TextInput(attrs={'class': "input-field"}),                  
+            }
--- school/urls.py
+++ school/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 app_name = 'school'
 urlpatterns = [
-    url(r'^$', views.school_list, name='school_list'),
-    url(r'^(?P<school_id>[0-9]+)/$', views.details, name='details'),
-    url(r'^add/$', views.add, name='add'),
-   # url(r'^(?P<school_id>[0-9]+)/delete/$', views.delete, name='delete'),      
+    url(r'^$', views.details, name='details'),
+    url(r'^edit/$', views.edit_school, name='edit'), 	
 ]
