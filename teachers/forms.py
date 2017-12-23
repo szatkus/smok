e963fa9aa6f5fdc7c35fd393bcca3a55be2b0f2b
surbanski <s.urbanski.h@gmail.com>
2017-12-23 18:52:00 +0100

    TSMOK-63: Powiazanie nauczyciel - klasa

--- teachers/forms.py
+++ teachers/forms.py
@@ -6,9 +6,10 @@ from django import forms
 class TeacherUpdateForm(forms.ModelForm):
 	class Meta:
 		model = Teacher
-		fields = ('first_name','last_name','subjects',)		
+		fields = ('first_name','last_name','subjects','groups',)
 		labels = {
 		'first_name': 'Imię',
 		'last_name': 'Nazwisko',
 		'subjects': 'Przedmioty',
+		'groups': 'Grupy',
 		}
--- teachers/urls.py
+++ teachers/urls.py
@@ -7,6 +7,7 @@ app_name = 'teachers'
 urlpatterns = [
     url(r'^$', login_required(views.TeachersList.as_view()),name='list'),
 	url(r'^(?P<pk>[0-9]+)/$', views.TeacherUpdate.as_view(),name='update'),
+	#url(r'^(?P<pk>[0-9]+)/$', views.edit_teacher, name='update'),
 	url(r'^add/$', views.TeacherCreate.as_view(),name='add'),
 	url(r'^(?P<pk>[0-9]+)/rm/$', views.TeacherDelete.as_view(),name='delete'),
 ]
