40af5308a6af7c67027745def3442084eafd4b65
surbanski <s.urbanski.h@gmail.com>
2017-12-31 17:10:17 +0100

    Naprawiony frontend po rozwizaniu konfliktow z modelem sali; Dodana wartosc domyslna dla szkoly w modelu planu zajec

--- classrooms/forms.py
+++ classrooms/forms.py
@@ -6,7 +6,7 @@ class ClassroomForm(forms.ModelForm):
     
     class Meta:
         model = Classroom
-        fields = ('name', 'building', 'seats', 'subjects', )
+        fields = ('name', 'building', 'seats', 'available_subjects', )
         widgets = {
             'name': forms.TextInput(attrs={'class': 'input-field'}),
             'building': forms.TextInput(attrs={'class': "input-field"}),         
--- classrooms/models.py
+++ classrooms/models.py
@@ -3,18 +3,13 @@ from django.db import models
 from school.models import School
 from subjects.models import Subject
 
+
 class Classroom(models.Model):
     class Meta:
         unique_together = (('name', 'building'),)
     name = models.CharField(max_length=200, verbose_name="Numer")
     building = models.CharField(max_length=200, verbose_name="Budynek")
     seats = models.IntegerField(default=0, verbose_name="Liczba miejsc")
-    subjects = models.CharField(max_length=200, verbose_name="Przedmioty")
-    available_subjects = models.ManyToManyField(Subject) 
-	
-    #subjects = models.ManyToManyField(Subject, blank=True)
-	#Szymon, hashuje to bo już dodałem relacje jako available_subjects i mogło by się rozsypać; Michał
-	
+    available_subjects = models.ManyToManyField(Subject, blank=True)
     school = models.ForeignKey(School, on_delete=models.CASCADE, verbose_name="Szkoła", default='-1' )
-    
 
